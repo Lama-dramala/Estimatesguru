ccc5d962a989b4052fb0d4d8a32ffd13bd3b9e8c
Lama-dramala <96291338+Lama-dramala@users.noreply.github.com>
2023-01-19 17:52:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const myCarouselElement = document.querySelector('#section2Carousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  touch: true,
+  ride: "carousel",
+})
+const mySecondCarouselElement = document.querySelector('#section6Carousel')
+
+const carouse2 = new bootstrap.Carousel(mySecondCarouselElement, {
+  interval: 5000,
+  touch: true,
+  ride: "carouse2",
+})
+
+const swiperSec9 = new Swiper('.section-9-swiper', {
+  slidesPerView: "auto",
+  spaceBetween: 30,  
+  navigation: {
+    nextEl: '#section-9-swiper-next',
+    prevEl: '#section-9-swiper-prev',
+  },
+});
+const swiperSec10 = new Swiper('.section-10-swiper', {
+  slidesPerView: "1",
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '#section-10-swiper-next',
+    prevEl: '#section-10-swiper-prev',
+  },
+  breakpoints: {
+    580: {
+      slidesPerView: "2",
+    },
+  }
+});
+
+const swiperSec11 = new Swiper('.section-11-swiper', {
+  slidesPerView: "auto",
+  loop: true,
+});
